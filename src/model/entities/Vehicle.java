48e0c6b0be02347f8d61f37ad46b326eb9e476d9
Ricardo Martins <rs84martins@gmail.com>
2020-07-07 22:32:28 -0300

    Projeto criado - sem interface

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,22 @@
+package model.entities;
+
+public class Vehicle {
+	
+	private String vehicle;
+	
+	public Vehicle() {
+	}
+	
+	public Vehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public String getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(String vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+}
